06a4a0b9ab3c7202832e3f585488d21326ded53a
JosephDeFacto <josip.mareljic@gmail.com>
2022-03-01 01:50:39 +0100

    set 'use' instead of using absolute path to instance

--- index.php
+++ index.php
@@ -6,13 +6,15 @@ ini_set("display_errors", true);
 require 'app/bootstrap.php';
 
 use App\Database\DatabaseConnection;
+use App\UserController;
+use App\UserRepository;
 
 $database = new DatabaseConnection();
 $db = $database->getConnection();
 
-$userRepository = new \App\UserRepository($database);
+$userRepository = new UserRepository($database);
 
-$controller = new \App\UserController($userRepository);
+$controller = new UserController($userRepository);
 
 $controller->addUser();
 
@@ -22,8 +24,6 @@ $controller->addUser();
 $id = array_key_exists('id', $_GET) ?? [];
 
 $userObject = $controller->getUser($id);
-
-
 
 
 
